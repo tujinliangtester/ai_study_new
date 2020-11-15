e4ee74a63d74b19ee4a625dd4f7157b015748d79
tujinliangtester <tujinliangtester@163.com>
2020-11-15 14:39:42 +0800

    手动写死算是运行起来了，但是训练结果损失函数不下降，最低都是8左右，后续尝试加深、jump等

---
+++ tensorflow_2_study/net_eight_lines/my_parse_cnn_layer.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import  numpy as np
+
+class My_parse_cnn_layer(tf.keras.layers.Layer):
+    def __init__(self, filters,
+                 kernel_size,
+                 strides=(1, 1),
+                 padding='valid'):
+        super(My_parse_cnn_layer, self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.units=kernel_size[0]*kernel_size[1]
+        self.strides = strides
+        self.padding = padding
+
+
+    def build(self, input_shape):
+        self.my_input_shape=input_shape
+        self.w = []
+        self.b = []
+        # 这里的input只处理单通道，即(N,W,H,C)=(N,W,H,1)
+        tmp_w=self.tmp_w = input_shape[1] // self.strides[0]
+        tmp_h=self.tmp_h = input_shape[2] // self.strides[1]
+        for i in range(tmp_w):
+            tmp_weight=[]
+            tmp_b=[]
+            for j in range(tmp_h):
+                tmp_weight.append(self.add_weight(
+                    shape=(self.units,),
+                    initializer="random_normal",
+                    trainable=True,
+                ))
+                tmp_b.append(self.add_weight(
+                    shape=(self.units,), initializer="random_normal", trainable=True
+                ))
+            self.w.append(tmp_weight)
+            self.b.append(tmp_b)
+    def call(self, inputs):
+        split_y=[]
+
+        for i in range(self.tmp_w):
+            for j in range(self.tmp_h):
+                tmp=self.my_draw(inputs,i,j)
+                w=self.w[i][j]
+                w=tf.reshape(w,(tmp.shape[-1],1))
+                split_y.append(tf.matmul(tmp,w)+self.b[i][j])
+        y=tf.concat(split_y,axis=-1)
+        return y
+
+    def my_draw(self,inputs,i,j):
+        tmp_i=i-1
+        tmp_j=j-1
+        res= None
+        for m in range(self.kernel_size[0]):
+            tmp_i+=1
+            for n in range(self.kernel_size[1]):
+                tmp_j += 1
+                if(self.my_juge_out(inputs,tmp_i,tmp_j)):
+                    # todo 报错！先写死一个批次的数量
+                    tmp=np.zeros(shape=(10000,1))
+                else:
+                    tmp=inputs[:,tmp_j,tmp_j]
+                if(res is None):
+                    res=tmp
+                else:
+                    res=tf.concat([res,tmp],axis=-1)
+        return res
+    def my_juge_out(self,inputs,i,j):
+        if(i<0 or j<0 or i>=inputs.shape[1] or j>=inputs.shape[2]):
+            return True
+        return False
+
+if __name__=='__main__':
+    w=tf.ones_initializer()
+    w=w((1,9))
+    print(w)
+    w=tf.transpose(w)
+    print(w)
